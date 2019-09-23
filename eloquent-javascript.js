ff286ea0cf36a75c60a4e91088243579e55fb6ca
ismetguzelgun <ismet.guzelgun@gmail.com>
2019-09-23 16:58:03 +0300

    Update eloquent-javascript.js

--- eloquent-javascript.js
+++ eloquent-javascript.js
@@ -8,3 +8,9 @@ console.log(NaN == NaN)
 // → false
 /*NaN is supposed to denote the result of a nonsensical computation, and as such, 
 it isn’t equal to the result of any other nonsensical computations.*/
+
+/*The difference in meaning between undefined and null is an accident of JavaScript’s design, 
+and it doesn’t matter most of the time. In cases where you actually have to concern yourself with these values, 
+I recommend treating them as mostly interchangeable.*/
+
+
