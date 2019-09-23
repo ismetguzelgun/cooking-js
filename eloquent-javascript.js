21613f2e35593e313ac1abb3e5c388fc221b0592
ismetguzelgun <ismet.guzelgun@gmail.com>
2019-09-23 16:43:37 +0300

    Update eloquent-javascript.js

--- eloquent-javascript.js
+++ eloquent-javascript.js
@@ -6,3 +6,5 @@
 //There is only one value in JavaScript that is not equal to itself, and that is NaN (“not a number”).
 console.log(NaN == NaN)
 // → false
+/*NaN is supposed to denote the result of a nonsensical computation, and as such, 
+it isn’t equal to the result of any other nonsensical computations.*/
