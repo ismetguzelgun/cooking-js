01333fdcaf42ea2a4b60f8565d18254c6b66c9c3
ismetguzelgun <ismet.guzelgun@gmail.com>
2019-12-10 21:44:55 +0300

    seppuku

---
+++ sportsstore/paketsavedev.sh
@@ -0,0 +1,11 @@
+#only dev
+
+npm install --save-dev json-server@0.14.2
+npm install --save-dev jsonwebtoken@8.1.1
+npm install --save-dev express@4.16.4
+npm install --save-dev express-graphql@0.7.1
+npm install --save-dev cors@2.8.5
+npm install --save-dev faker@4.1.0
+npm install --save-dev chokidar@2.0.4
+npm install --save-dev npm-run-all@4.1.3
+npm install --save-dev connect-history-api-fallback@1.5.0
